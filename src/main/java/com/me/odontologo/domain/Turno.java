793aa3fa3f3d8631eb3bcc7c7b9dc5be0ef7ffbb
juancruzmarzetti <naujmarz@gmail.com>
2024-03-05 21:39:36 -0300

    turno-class

--- src/main/java/com/me/odontologo/domain/Turno.java
+++ src/main/java/com/me/odontologo/domain/Turno.java
@@ -1,5 +1,39 @@
 package com.me.odontologo.domain;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Turno {
-    //TODO
+    private int id;
+    private LocalDate fecha;
+    private LocalTime hora;
+
+    public Turno(int id, LocalDate fecha, LocalTime hora) {
+        this.id = id;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
 }
