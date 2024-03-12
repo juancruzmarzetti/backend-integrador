d498ffd74aa2706519950b9f666098c0e1fba9f5
juancruzmarzetti <naujmarz@gmail.com>
2024-03-11 21:38:15 -0300

    html

--- src/main/java/com/me/odontologo/controllers/TurnoController.java
+++ src/main/java/com/me/odontologo/controllers/TurnoController.java
@@ -1,10 +1,39 @@
 package com.me.odontologo.controllers;
 
+import com.me.odontologo.model.Turno;
+import com.me.odontologo.services.IOdontologoService;
+import com.me.odontologo.services.IPacienteService;
+import com.me.odontologo.services.ITurnoService;
+import com.me.odontologo.services.implementations.OdontologoService;
+import com.me.odontologo.services.implementations.PacienteService;
+import com.me.odontologo.services.implementations.TurnoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("domicilios")
+@RequestMapping("turnos")
 public class TurnoController {
-    //TODO
+    private ITurnoService turnoService;
+    private IOdontologoService odontologoService;
+    private IPacienteService pacienteService;
+    public TurnoController(PacienteService pacienteService,
+                           OdontologoService odontologoService,
+                           TurnoService turnoService){
+        this.pacienteService = pacienteService;
+        this.odontologoService = odontologoService;
+        this.turnoService = turnoService;
+    }
+
+    @PostMapping("/agregar")
+    public ResponseEntity<Turno> agregar(@RequestBody Turno turno){
+        ResponseEntity<Turno> response;
+        if(){
+
+        }
+
+        return response;
+    }
 }
