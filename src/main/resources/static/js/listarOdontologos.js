97a45251bbdb97cb831410b30c3c322ab62dee25
juancruzmarzetti <naujmarz@gmail.com>
2024-03-22 21:54:13 -0300

    avances-vista-2

--- src/main/resources/static/js/listarOdontologos.js
+++ src/main/resources/static/js/listarOdontologos.js
@@ -1,44 +1,63 @@
+//El evento load se ejecuta al cargar la página que muestra la lista de películas
 window.addEventListener('load', function () {
-    (function(){
+     //con fetch invocamos a la API de peliculas con el método GET
+     //nos devolverá un JSON con una colección de películas
+     const url = '/odontologos/all';
+     const settings = {
+       method: 'GET'
+     }
+     fetch(url,settings)
+     .then(response => response.json())
+     .then(data => {
+     //recorremos la colección de odontologos del JSON
+        for(odontologo of data){
 
-      //con fetch invocamos a la API de odontólogos con el método GET
-      //nos devolverá un JSON con una colección de odontólogos
-      const url = '/odontologos/all';
-      const settings = {
-        method: 'GET'
-      }
+           //por cada película armaremos una fila de la tabla
+           //cada fila tendrá un ID que luego nos permitirá
+           //borrar la fila si eliminamos la película
 
-      fetch(url,settings)
-      .then(response => response.json())
-      .then(data => {
-      //recorremos la colección de odontólogos del JSON
-         for(dentist of data){
-            //por cada odontólogo armaremos una fila de la tabla
-            //cada fila tendrá un id que luego nos permitirá borrar la fila si eliminamos el odontólogo
-            var table = document.getElementById("dentistTable");
-            var dentistRow = table.insertRow();
-            let tr_id = 'tr_' + dentist.id;
-            dentistRow.id = tr_id;
+           var table = document.getElementById("peliculaTable");
+           var peliculaRow =table.insertRow();
+           let tr_id = 'tr_' + odontologo.id;
+           peliculaRow.id = tr_id;
 
-            dentistRow.innerHTML =
-                    '<td class=\"td_id\">' + dentist.id + '</td>' +
-                    '<td class=\"td_nombre\">' + dentist.nombre.toUpperCase() + '</td>' +
-                    '<td class=\"td_apellido\">' + dentist.apellido.toUpperCase() + '</td>' +
-                    '<td class=\"td_matricula\">' + dentist.matricula + '</td>'
 
-                    ;
 
-        };
+           //por cada película creamos un botón delete que
+           //agregaremos en cada fila para poder eliminar la misma
+           //dicho botón invocará a la función de JavaScript deleteByKey que se encargará
+           //de llamar a la API para eliminar una película
 
+           let deleteButton = '<button' +
+                                     ' id=' + '\"' + 'btn_delete_' + odontologo.id + '\"' +
+                                     ' type="button" onclick="deleteBy('+ odontologo.id +')"' +
+                                     'class="btn btn-danger btn_delete">' +
+                                     '&times' +
+                                     '</button>';
+
+           //por cada película creamos un botón que muestra el ID
+           //y que al hacerle clic invocará a la función de JavaScript findBy
+           //que se encargará de buscar la película que queremos modificar
+           //y mostrar los datos de la misma en un formulario.
+
+           let updateButton = '<button' +
+                                     ' id=' + '\"' + 'btn_id_' + odontologo.id + '\"' +
+                                     ' type="button" onclick="findBy('+ odontologo.id +')"' +
+                                     ' class="btn btn-info btn_id">' +
+                                     odontologo.id +
+                                     '</button>';
+
+
+
+           //armamos cada columna de la fila
+           //como primera columna pondremos el botón modificar
+           //luego los datos de la película
+           //como última columna, el botón eliminar
+           peliculaRow.innerHTML = '<td>' + updateButton + '</td>' +
+                 '<td class=\"td_matricula\">' + odontologo.matricula + '</td>' +
+                 '<td class=\"td_nombre\">' + odontologo.nombre + '</td>' +
+                 '<td class=\"td_apellido\">' + odontologo.apellido + '</td>' +
+                 '<td>' + deleteButton + '</td>';
+       };
     })
-    })
-
-    /*(function(){
-      let pathname = window.location.pathname;
-      if (pathname == "/asdasdasd.html") {
-          document.querySelector(".nav .nav-item a:last").addClass("active");
-      }
-    })*/
-
-
-    })
+})
--- src/test/java/com/me/odontologo/service/implementation/OdontologoServiceTest.java
+++ src/test/java/com/me/odontologo/service/implementation/OdontologoServiceTest.java
@@ -1,66 +1,96 @@
 package com.me.odontologo.service.implementation;
 
 import com.me.odontologo.entity.Odontologo;
-import org.aspectj.lang.annotation.Before;
+import com.me.odontologo.service.IOdontologoService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class OdontologoServiceTest {
-    /*
-    @Autowired
-    private OdontologoService odontologoService;
 
-    @BeforeAll
-    public static void setUp() {
-        Odontologo odontologo1 = new Odontologo();
-        Odontologo odontologo2 = new Odontologo();
-    }
+    @Autowired
+    private IOdontologoService odontologoService;
 
     @Test
     void guardarOdontologo() {
+        Odontologo odontologo1 = new Odontologo();
+        odontologo1.setMatricula(1111111);
+        odontologo1.setNombre("Jorge");
+        odontologo1.setApellido("Messi");
+
         Odontologo odontologoGuardado = odontologoService.guardarOdontologo(odontologo1);
-        //Assertions.assertEquals();
+
+        Assertions.assertEquals(odontologo1.getMatricula(), odontologoGuardado.getMatricula());
+
+        odontologoService.eliminarOdontologo(odontologoGuardado.getId());
     }
 
     @Test
     void eliminarOdontologo() {
-        Odontologo odontologo1 = new Odontologo(1, "Jorge", "Lopez");
-        Odontologo odontologo2 = new Odontologo(2, "Lionel", "Messi");
+        Odontologo odontologo1 = new Odontologo();
+        Odontologo odontologo2 = new Odontologo();
+
+        odontologo1.setMatricula(1111111);
+        odontologo1.setNombre("Jorge");
+        odontologo1.setApellido("Messi");
+
+        odontologo2.setMatricula(22222222);
+        odontologo2.setNombre("Lionel");
+        odontologo2.setApellido("Martinez");
 
         Odontologo odontologoAEliminar = odontologoService.guardarOdontologo(odontologo1);
         odontologoService.guardarOdontologo(odontologo2);
         odontologoService.eliminarOdontologo(odontologoAEliminar.getId());
 
         List<Odontologo> odontologos = odontologoService.buscarTodosLosOdontologos();
+
         Assertions.assertEquals(1, odontologos.size());
+
+        odontologoService.eliminarOdontologo(odontologo2.getId());
     }
 
     @Test
     void buscarTodosLosOdontologos() {
-        Odontologo odontologo1 = new Odontologo(1, "Jorge", "Lopez");
-        Odontologo odontologo2 = new Odontologo(2, "Lionel", "Messi");
+        Odontologo odontologo1 = new Odontologo();
+        Odontologo odontologo2 = new Odontologo();
+
+        odontologo1.setMatricula(1111111);
+        odontologo1.setNombre("Jorge");
+        odontologo1.setApellido("Messi");
+
+        odontologo2.setMatricula(22222222);
+        odontologo2.setNombre("Lionel");
+        odontologo2.setApellido("Martinez");
 
         odontologoService.guardarOdontologo(odontologo1);
         odontologoService.guardarOdontologo(odontologo2);
 
         List<Odontologo> odontologos = odontologoService.buscarTodosLosOdontologos();
+
         Assertions.assertEquals(2, odontologos.size());
+
+        odontologoService.eliminarOdontologo(odontologo1.getId());
+        odontologoService.eliminarOdontologo(odontologo2.getId());
     }
 
     @Test
     void buscar() {
-        Odontologo odontologo1 = new Odontologo(1, "Jorge", "Lopez");
+        Odontologo odontologo1 = new Odontologo();
+        odontologo1.setMatricula(1111111);
+        odontologo1.setNombre("Jorge");
+        odontologo1.setApellido("Messi");
 
         Odontologo odontologoABuscar = odontologoService.guardarOdontologo(odontologo1);
-        Odontologo odontologoBuscado = odontologoService.buscar(odontologoABuscar.getId());
+        Optional<Odontologo> odontologoBuscado = odontologoService.buscar(odontologoABuscar.getId());
 
-        Assertions.assertEquals(odontologoABuscar.getId(), odontologoBuscado.getId());
+        Assertions.assertTrue(odontologoBuscado.isPresent());
+
+        odontologoService.eliminarOdontologo(odontologoABuscar.getId());
     }
-     */
+
 }
